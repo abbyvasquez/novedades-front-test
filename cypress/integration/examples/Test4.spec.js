951ed6cb1990e58c7640a59759a245378351a59a
Pablo Restrepo <pablofrg98@gmail.com>
2020-01-31 16:10:27 -0500

    se eliminan test inecesarios

--- cypress/integration/examples/Test4.spec.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="Cypress" />
-describe('My First Test Suite', function() 
-{
-  it('My FirstTest case',function() 
-  {
-    cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
-    cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','Option1')
-  })
-})
