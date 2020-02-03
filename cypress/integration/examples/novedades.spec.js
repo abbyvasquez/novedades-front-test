f17df52d5ac505b87b482aa66648478fd5a7d139
edison.vasquez@sofka.com.co <edison.vasquez@sofka.com.co>
2020-02-03 17:57:38 -0500

    scripts novedades superiores con soluciones y aun no guarda

--- cypress/integration/examples/novedades.spec.js
+++ cypress/integration/examples/novedades.spec.js
@@ -1,79 +1,84 @@
 describe('Novedades de recogidas', function() {
 
     it('Novedades superiores - Cruzar contra faltante CM', function() {
-    cy.visit("https://app-dot-cm-novedades-checkpoint-dev.appspot.com/#/login");
-    cy.get('#mat-input-0').type('olgav')
-    cy.get('#mat-input-1').type('olgav')
+    cy.visit("https://app-dot-cm-novedades-checkpoint-test.appspot.com/#/login");
+    cy.get('#mat-input-0').type('pruebas')
+    cy.get('#mat-input-1').type('pruebas')
     cy.get('.mat-icon').click()
     cy.get('.app-button').click()
-    cy.get('.mat-select-placeholder').wait(2000).click()
-    cy.get('#mat-option-0').click()
+     cy.get('.dashboard-table').find('.clickable-element-row').contains(' 77 Envigado Centro Integral City Plaza ').click()
  
     cy.get('.novelties-index-container').find('.novelty-colum').find('.list-novelty').wait(2000).find('.list-card-novelty').each(($el,index,$list)=>{  
     const NumberGuia=$el.find('h3.parcel-number').text()
-        if (NumberGuia.includes('20025027564'))
+        if (NumberGuia.includes('03200098240'))
         {
          $el.find('button').first().click()
         }
       })
       cy.get('.mat-menu-content').find('.mat-menu-item').contains('Cruzar contra faltante CM').click()
-     })
+      cy.get('#mat-input-2').click().type('comentario')
+      cy.get('.app-button').click()
+    })
 
      it('Novedades superiores - Cruzar contra faltante cliente', function() {
-      cy.visit("https://app-dot-cm-novedades-checkpoint-dev.appspot.com/#/login");
-      cy.get('#mat-input-0').type('olgav')
-      cy.get('#mat-input-1').type('olgav')
+      cy.visit("https://app-dot-cm-novedades-checkpoint-test.appspot.com/#/login");
+      cy.get('#mat-input-0').type('pruebas')
+      cy.get('#mat-input-1').type('pruebas')
       cy.get('.mat-icon').click()
       cy.get('.app-button').click()
-      cy.get('.mat-select-placeholder').wait(2000).click()
-      cy.get('#mat-option-0').click()
-   
+      cy.get('.dashboard-table').find('.clickable-element-row').contains(' 77 Envigado Centro Integral City Plaza ').click()   
       cy.get('.novelties-index-container').find('.novelty-colum').find('.list-novelty').wait(2000).find('.list-card-novelty').each(($el,index,$list)=>{  
       const NumberGuia=$el.find('h3.parcel-number').text()
-          if (NumberGuia.includes('20025027564'))
+          if (NumberGuia.includes('03200098242'))
           {
            $el.find('button').first().click()
           }
         })
-        cy.get('.mat-menu-content').find('.mat-menu-item').contains('Cruzar contra faltante cliente').click()  
+        cy.get('.mat-menu-content').find('.mat-menu-item').contains('Cruzar contra faltante cliente').click()
+        cy.get('#mat-input-2').click().type('comentario')
+        cy.get('.app-button').click()
        })
 
        it('Novedades superiores - Devolver/cambio destino', function() {
-        cy.visit("https://app-dot-cm-novedades-checkpoint-dev.appspot.com/#/login");
-        cy.get('#mat-input-0').type('olgav')
-        cy.get('#mat-input-1').type('olgav')
+        cy.visit("https://app-dot-cm-novedades-checkpoint-test.appspot.com/#/login");
+        cy.get('#mat-input-0').type('pruebas')
+        cy.get('#mat-input-1').type('pruebas')
         cy.get('.mat-icon').click()
         cy.get('.app-button').click()
-        cy.get('.mat-select-placeholder').wait(2000).click()
-        cy.get('#mat-option-0').click()
-     
+        cy.get('.dashboard-table').find('.clickable-element-row').contains(' 77 Envigado Centro Integral City Plaza ').click()     
         cy.get('.novelties-index-container').find('.novelty-colum').find('.list-novelty').wait(2000).find('.list-card-novelty').each(($el,index,$list)=>{  
         const NumberGuia=$el.find('h3.parcel-number').text()
-            if (NumberGuia.includes('20025027564'))
+            if (NumberGuia.includes('03200098239'))
             {
              $el.find('button').first().click()
             }
           })
+          
           cy.get('.mat-menu-content').find('.mat-menu-item').contains('Devolver/cambio destino').click()  
-         })
-
+          cy.get('input[name="name"]').click().type('name')
+          cy.get('input[name="lastName"]').click().type('lastName')         
+          cy.get('#mat-input-2').click().type('comentario')
+          cy.get('.app-button').click()
+        })
 
          it('Novedades superiores - Entregar sobrante a destinatario', function() {
-          cy.visit("https://app-dot-cm-novedades-checkpoint-dev.appspot.com/#/login");
-          cy.get('#mat-input-0').type('olgav')
-          cy.get('#mat-input-1').type('olgav')
+          cy.visit("https://app-dot-cm-novedades-checkpoint-test.appspot.com/#/login");
+          cy.get('#mat-input-0').type('pruebas')
+          cy.get('#mat-input-1').type('pruebas')
           cy.get('.mat-icon').click()
           cy.get('.app-button').click()
-          cy.get('.mat-select-placeholder').wait(2000).click()
-          cy.get('#mat-option-0').click()
-       
+          cy.get('.dashboard-table').find('.clickable-element-row').contains(' 77 Envigado Centro Integral City Plaza ').click()       
           cy.get('.novelties-index-container').find('.novelty-colum').find('.list-novelty').wait(2000).find('.list-card-novelty').each(($el,index,$list)=>{  
           const NumberGuia=$el.find('h3.parcel-number').text()
-              if (NumberGuia.includes('20025027564'))
+              if (NumberGuia.includes('03200098244'))
               {
                $el.find('button').first().click()
               }
             })
             cy.get('.mat-menu-content').find('.mat-menu-item').contains('Entregar sobrante a destinatario').click()  
-           })
+            cy.get('input[name="name"]').click().type('name')
+            cy.get('input[name="lastName"]').click().type('lastName')
+            cy.get('#mat-input-2').click().type('comentario')
+            cy.get('.app-button').click()
+          })
     })
